d808fe028e394ff498c72f6c7dc6f02fa752d804
AD <burningggflamesss@gmail.com>
2026-01-25 21:19:18 +0545

    Add GSAP and Lenis for smooth scrolling

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,16 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { useEffect } from 'react'
+import Lenis from 'lenis'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import appCss from '../styles.css?url'
 import "../styles/globals.css"
 import { Toaster } from "@/components/ui/sonner"
 import Header from '@/components/global/Header'
 
+gsap.registerPlugin(ScrollTrigger)
 
 export const Route = createRootRoute({
   head: () => ({
@@ -33,6 +38,53 @@ export const Route = createRootRoute({
 })
 
 function RootDocument({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const scroller = document.documentElement
+    const lenis = new Lenis()
+
+    ScrollTrigger.scrollerProxy(scroller, {
+      scrollTop(value) {
+        if (value !== undefined)
+          lenis.scrollTo(value, { immediate: true })
+
+        return lenis.scroll || window.scrollY
+      },
+
+      getBoundingClientRect() {
+        return {
+          top: 0,
+          left: 0,
+          width: window.innerWidth,
+          height: window.innerHeight
+        }
+      },
+      pinType: scroller.style.transform ? "transform" : "fixed"
+    })
+
+    ScrollTrigger.defaults({ scroller })
+
+    const onTick = (time: number) => {
+      lenis.raf(time * 2000)
+    }
+
+    const onLenisScroll = () => {
+      ScrollTrigger.update()
+    }
+
+    lenis.on('scroll', onLenisScroll)
+    gsap.ticker.add(onTick)
+    gsap.ticker.lagSmoothing(0)
+
+
+    ScrollTrigger.refresh()
+
+    return () => {
+      ScrollTrigger.scrollerProxy(scroller, {})
+      gsap.ticker.remove(onTick)
+      lenis.off('scroll', onLenisScroll)
+      lenis.destroy()
+    }
+  }, [])
   return (
     <html lang="en">
       <head>
@@ -41,18 +93,18 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body>
         <Header></Header>
         <Toaster />
-          {children}
-          <TanStackDevtools
-            config={{
-              position: 'bottom-right',
-            }}
-            plugins={[
-              {
-                name: 'Tanstack Router',
-                render: <TanStackRouterDevtoolsPanel />,
-              },
-            ]}
-          />
+        {children}
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+          ]}
+        />
         <Scripts />
       </body>
     </html>
